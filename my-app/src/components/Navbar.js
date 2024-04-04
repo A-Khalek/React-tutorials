d58d745f390f94278aa0c7f3a80fd52dd7d4b7f3
Md. Abdul Khalek <73632469+A-Khalek@users.noreply.github.com>
2024-04-04 09:07:44 +0600

    add login in menu

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -8,6 +8,8 @@ export default function Navbar(){
                 <li> <NavLink to="/">  Home</NavLink> </li>
                 <li> <NavLink to="/about" >About</NavLink> </li>
                 <li> <NavLink to="/servics">  Servics</NavLink> </li>
+                <li> <NavLink to="/post/:category/:subcategory">  Post</NavLink> </li>
+                <li> <NavLink to="/login">  Login</NavLink> </li>
             </ul>
         </nav>
     );
