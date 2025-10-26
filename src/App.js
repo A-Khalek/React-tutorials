ac10190c9df599714c549bd50eee18de0ef826da
Md. Abdul Khalek <abdul.khalek@uttarauniversity.edu.bd>
2025-10-26 19:56:35 +0600

    incress and decress using useState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
-import { BrowserRouter as Router, Routes,Route, Navigate } from 'react-router-dom';
-import './index.css';
-import Home from './components/Home';
-import About from './components/About';
-import Error from './components/Error'
-import Services from './components/Services'
-import Navabr from './components/Navbar'
-import Post from './components/Post'
-import Login from './components/Login'
-import Card from './components/Card'
-import Button from './components/Button/Button';
-import Student from './components/Student';
-import UserGreeting from './components/UserGreeting';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Error from "./components/Error";
+import Services from "./components/Services";
+import Navabr from "./components/Navbar";
+import Post from "./components/Post";
+import Login from "./components/Login";
+import Card from "./components/Card";
+import Button from "./components/Button/Button";
+import Student from "./components/Student";
+import UserGreeting from "./components/UserGreeting";
 
 function App() {
   const isLogedIn = true;
   return (
     <>
+      {/* <UserGreeting isLoggedIn={true} name="Raiyan" />
+      <Student name="Raiyan" age={2} isStudent={true} />
+      <Student name="Raiyan" age={20} isStudent={false} />
+      <Student />
+      <Card />
+      <Button /> */}
 
-    <UserGreeting isLoggedIn = {true} name="Raiyan"/>
-    <Student name="Raiyan" age={2} isStudent={true}/>
-    <Student name="Raiyan" age={20} isStudent={false}/>
-    <Student/>
-    <Card/>
-    <Button/>
-
-    <Router>
-    <Navabr/>
-      <Routes>
+      <Router>
+        <Navabr />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/servics" element={<Services number="5" />} />
-          <Route path="/post/:category/:subcategory" element={<Post/>} />   this is send route parameters
-          <Route path="/login" element={ isLogedIn ? <Login/> : <Home />} />
+          <Route path="/post/:category/:subcategory" element={<Post />} /> this
+          is send route parameters
+          <Route path="/login" element={isLogedIn ? <Login /> : <Home />} />
           <Route path="*" element={<Error />} />
         </Routes>
-    </Router>
+      </Router>
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Home() {
-    return (
-        <div>
-            <div>This is Home Page</div>
-        </div>
-    );
+  const [number, setNumber] = useState(0);
+
+  const incress = () => {
+    if (number < 5) {
+      setNumber(number + 1);
+    }
+  };
+
+  const decress = () => {
+    if (number > 0) {
+      setNumber(number - 1);
+    }
+  };
+
+  const reset = () => {
+    setNumber(0);
+  };
+
+  return (
+    <>
+      <h1> Number is : {number}</h1>
+
+      <button class="btn btn-primay" onClick={incress}>
+        Incress
+      </button>
+
+      <button onClick={decress}> Decress </button>
+
+      <button onClick={reset}>Reset</button>
+    </>
+  );
 }
 
 export default Home;
